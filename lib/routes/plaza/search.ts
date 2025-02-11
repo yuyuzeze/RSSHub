8b962c2af9eda47dc17e659b58b3c28e0a6ef487
yuyuzeze <yuyuzeze@gmail.com>
2025-02-11 18:50:28 +0900

    plaza 路由

---
+++ lib/routes/plaza/search.ts
@@ -0,0 +1,55 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+import { parseDate } from '@/utils/parse-date';
+
+export const route: Route = {
+    path: '/search',
+    categories: ['shopping'],
+    example: '/plaza/search.aspx?ttl=All%20%E6%96%B0%E5%95%86%E5%93%81&s=seq,-s_dt&genre=all',
+    name: '商品一覧',
+    maintainers: ['yuyuzeze'],
+    description: ``,
+    handler,
+};
+
+async function handler(ctx) {
+    const queryParams = ctx.req.query();
+    let url = 'https://plazastyle.search.zetacx.net/api/item';
+
+    // 将所有查询参数添加到URL
+    const queryString = new URLSearchParams(queryParams).toString();
+    if (queryString) {
+        url += `?${queryString}`;
+        url += `&timestamp=${Date.now()}`;
+    }
+
+    const { data } = await got({
+        method: 'get',
+        url,
+    });
+
+    const jsonData = JSON.parse(data);
+
+    const items = jsonData.result.items.map((item) => ({
+        title: item.name,
+        link: `https://www.plazastyle.com/shop/g/g${item.goods}`,
+        image: `https://www.plazastyle.com/img/goods/L/${item.image_url}`,
+        description: `
+            <div>
+                <img src="https://www.plazastyle.com/images/sys/item/${item.image_url}" />
+                <p>価格: ￥${item.price}</p>
+                <p>ブランド: ${item.rits_brand_name || ''}</p>
+                ${item.new ? '<p>新商品</p>' : ''}
+                ${item.sale ? '<p>セール中</p>' : ''}
+            </div>
+        `,
+        category: item.category,
+        pubDate: parseDate(item.release_dt),
+    }));
+
+    return {
+        title: `PLAZA - ${jsonData.request_params.q || '全品类'}`,
+        link: url,
+        item: items,
+    };
+}
