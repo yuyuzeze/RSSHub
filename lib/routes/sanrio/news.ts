b55ed1ba0e1cc7e7164b52219ba8e8498aa29f2d
yuyuzeze <yuyuzeze@gmail.com>
2025-02-13 14:48:59 +0900

    sanrio 路由

---
+++ lib/routes/sanrio/news.ts
@@ -0,0 +1,47 @@
+import { Route } from '@/types';
+import got from '@/utils/got';
+
+export const route: Route = {
+    path: '/news/:category',
+    categories: ['shopping'],
+    example: '/goods/?categories=34&pg=1',
+    name: 'Goods',
+    maintainers: ['yuyuzeze'],
+    description: ``,
+    handler,
+};
+
+async function handler(ctx) {
+    const queryParams = ctx.req.query();
+    const category = ctx.req.params.category;
+    let url = 'https://www.sanrio.co.jp/wp-json/wp/v2/news';
+
+    // 将所有查询参数添加到URL
+    const queryString = new URLSearchParams(queryParams).toString();
+    if (queryString) {
+        url += `?${queryString}&boss_category=${category}`;
+    }
+
+    const { data } = await got({
+        method: 'get',
+        url,
+    });
+
+    const items = data.map((item) => {
+        const categories = item.category.map(c => c.name);
+
+        return {
+            title: item.title.rendered,
+            link: item.link,
+            category: categories,
+            image: item.thumbnail.url,
+            description: `<div><img src="${item.thumbnail.url}" /></div>`
+        }
+    });
+
+    return {
+        title: 'Sanrio - News',
+        link: url,
+        item: items,
+    };
+}
